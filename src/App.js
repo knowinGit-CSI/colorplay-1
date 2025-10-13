27167641a3e738f6ffe943d590f627d558f30d33
Abhinavpapini <papiniabhinav@gmail.com>
2025-10-13 12:12:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import React from 'react';
+import './index.css';
+
+// Import all 65 bubble components
+import Bubble01 from './components/bubbles/Bubble01';
+import Bubble02 from './components/bubbles/Bubble02';
+import Bubble03 from './components/bubbles/Bubble03';
+import Bubble04 from './components/bubbles/Bubble04';
+import Bubble05 from './components/bubbles/Bubble05';
+import Bubble06 from './components/bubbles/Bubble06';
+import Bubble07 from './components/bubbles/Bubble07';
+import Bubble08 from './components/bubbles/Bubble08';
+import Bubble09 from './components/bubbles/Bubble09';
+import Bubble10 from './components/bubbles/Bubble10';
+import Bubble11 from './components/bubbles/Bubble11';
+import Bubble12 from './components/bubbles/Bubble12';
+import Bubble13 from './components/bubbles/Bubble13';
+import Bubble14 from './components/bubbles/Bubble14';
+import Bubble15 from './components/bubbles/Bubble15';
+import Bubble16 from './components/bubbles/Bubble16';
+import Bubble17 from './components/bubbles/Bubble17';
+import Bubble18 from './components/bubbles/Bubble18';
+import Bubble19 from './components/bubbles/Bubble19';
+import Bubble20 from './components/bubbles/Bubble20';
+import Bubble21 from './components/bubbles/Bubble21';
+import Bubble22 from './components/bubbles/Bubble22';
+import Bubble23 from './components/bubbles/Bubble23';
+import Bubble24 from './components/bubbles/Bubble24';
+import Bubble25 from './components/bubbles/Bubble25';
+import Bubble26 from './components/bubbles/Bubble26';
+import Bubble27 from './components/bubbles/Bubble27';
+import Bubble28 from './components/bubbles/Bubble28';
+import Bubble29 from './components/bubbles/Bubble29';
+import Bubble30 from './components/bubbles/Bubble30';
+import Bubble31 from './components/bubbles/Bubble31';
+import Bubble32 from './components/bubbles/Bubble32';
+import Bubble33 from './components/bubbles/Bubble33';
+import Bubble34 from './components/bubbles/Bubble34';
+import Bubble35 from './components/bubbles/Bubble35';
+import Bubble36 from './components/bubbles/Bubble36';
+import Bubble37 from './components/bubbles/Bubble37';
+import Bubble38 from './components/bubbles/Bubble38';
+import Bubble39 from './components/bubbles/Bubble39';
+import Bubble40 from './components/bubbles/Bubble40';
+import Bubble41 from './components/bubbles/Bubble41';
+import Bubble42 from './components/bubbles/Bubble42';
+import Bubble43 from './components/bubbles/Bubble43';
+import Bubble44 from './components/bubbles/Bubble44';
+import Bubble45 from './components/bubbles/Bubble45';
+import Bubble46 from './components/bubbles/Bubble46';
+import Bubble47 from './components/bubbles/Bubble47';
+import Bubble48 from './components/bubbles/Bubble48';
+import Bubble49 from './components/bubbles/Bubble49';
+import Bubble50 from './components/bubbles/Bubble50';
+import Bubble51 from './components/bubbles/Bubble51';
+import Bubble52 from './components/bubbles/Bubble52';
+import Bubble53 from './components/bubbles/Bubble53';
+import Bubble54 from './components/bubbles/Bubble54';
+import Bubble55 from './components/bubbles/Bubble55';
+import Bubble56 from './components/bubbles/Bubble56';
+import Bubble57 from './components/bubbles/Bubble57';
+import Bubble58 from './components/bubbles/Bubble58';
+import Bubble59 from './components/bubbles/Bubble59';
+import Bubble60 from './components/bubbles/Bubble60';
+import Bubble61 from './components/bubbles/Bubble61';
+import Bubble62 from './components/bubbles/Bubble62';
+import Bubble63 from './components/bubbles/Bubble63';
+import Bubble64 from './components/bubbles/Bubble64';
+import Bubble65 from './components/bubbles/Bubble65';
+
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white py-8 px-4">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-4xl font-bold text-center text-black mb-4">
+          ColorPlay - Color Burst Effect
+        </h1>
+        
+        {/* Responsive bubble grid layout */}
+        <div className="flex flex-wrap justify-center items-center gap-6 p-8">
+          <Bubble01 />
+          <Bubble02 />
+          <Bubble03 />
+          <Bubble04 />
+          <Bubble05 />
+          <Bubble06 />
+          <Bubble07 />
+          <Bubble08 />
+          <Bubble09 />
+          <Bubble10 />
+          <Bubble11 />
+          <Bubble12 />
+          <Bubble13 />
+          <Bubble14 />
+          <Bubble15 />
+          <Bubble16 />
+          <Bubble17 />
+          <Bubble18 />
+          <Bubble19 />
+          <Bubble20 />
+          <Bubble21 />
+          <Bubble22 />
+          <Bubble23 />
+          <Bubble24 />
+          <Bubble25 />
+          <Bubble26 />
+          <Bubble27 />
+          <Bubble28 />
+          <Bubble29 />
+          <Bubble30 />
+          <Bubble31 />
+          <Bubble32 />
+          <Bubble33 />
+          <Bubble34 />
+          <Bubble35 />
+          <Bubble36 />
+          <Bubble37 />
+          <Bubble38 />
+          <Bubble39 />
+          <Bubble40 />
+          <Bubble41 />
+          <Bubble42 />
+          <Bubble43 />
+          <Bubble44 />
+          <Bubble45 />
+          <Bubble46 />
+          <Bubble47 />
+          <Bubble48 />
+          <Bubble49 />
+          <Bubble50 />
+          <Bubble51 />
+          <Bubble52 />
+          <Bubble53 />
+          <Bubble54 />
+          <Bubble55 />
+          <Bubble56 />
+          <Bubble57 />
+          <Bubble58 />
+          <Bubble59 />
+          <Bubble60 />
+          <Bubble61 />
+          <Bubble62 />
+          <Bubble63 />
+          <Bubble64 />
+          <Bubble65 />
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/bubbles/Bubble40.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Bubble40 = ({ title = "button - 40" }) => {
+  return (
+    <div className="relative group cursor-pointer">
+      {/* Outer burst rings - appear on hover */}
+      <div className="absolute inset-0 w-24 h-24 rounded-full bg-gradient-to-r from-cyan-500 to-sky-500 opacity-0 group-hover:opacity-60 scale-100 group-hover:scale-[4] transition-all duration-1000 ease-out blur-sm"></div>
+      <div className="absolute inset-0 w-24 h-24 rounded-full bg-gradient-to-r from-cyan-400 to-sky-400 opacity-0 group-hover:opacity-40 scale-100 group-hover:scale-[3] transition-all duration-800 ease-out blur-sm delay-100"></div>
+      <div className="absolute inset-0 w-24 h-24 rounded-full bg-gradient-to-r from-cyan-300 to-sky-300 opacity-0 group-hover:opacity-30 scale-100 group-hover:scale-[2] transition-all duration-600 ease-out blur-sm delay-200"></div>
+      
+      {/* Main bubble */}
+      <div className="relative z-10 bg-gradient-to-br from-cyan-500 to-cyan-700 w-24 h-24 rounded-full shadow-xl hover:shadow-2xl transition-all duration-500 ease-out hover:scale-110 flex items-center justify-center group-hover:animate-pulse">
+        <span className="text-white text-xs font-bold text-center px-2 drop-shadow-lg">{title}</span>
+        
+        {/* Inner sparkle effect */}
+        <div className="absolute inset-2 rounded-full bg-gradient-to-t from-transparent to-white opacity-0 group-hover:opacity-30 transition-opacity duration-500"></div>
+      </div>
+      
+      {/* Particle effects */}
+      <div className="absolute top-2 left-2 w-1 h-1 bg-sky-300 rounded-full opacity-0 group-hover:opacity-100 group-hover:animate-ping"></div>
+      <div className="absolute top-4 right-3 w-1 h-1 bg-white rounded-full opacity-0 group-hover:opacity-100 group-hover:animate-ping delay-100"></div>
+      <div className="absolute bottom-3 left-4 w-1 h-1 bg-cyan-300 rounded-full opacity-0 group-hover:opacity-100 group-hover:animate-ping delay-200"></div>
+      <div className="absolute bottom-2 right-2 w-1 h-1 bg-sky-200 rounded-full opacity-0 group-hover:opacity-100 group-hover:animate-ping delay-300"></div>
+    </div>
+  );
+};
+
+export default Bubble40;
